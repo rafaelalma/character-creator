0a1d8c43be9425fcd7b79776cf4af4f9a265f0bf
rafaelalma <rafaelalma92@gmail.com>
2023-05-10 19:37:45 +0200

    style(character editor): add perspective

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -25,6 +25,8 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.perspective} />
+
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
@@ -71,18 +73,18 @@ function App() {
             handleSelectOption={setClothesColor}
           />
         </div>
-
-        <div className={styles.characterWrapper}>
-          <Character
-            body={body}
-            head={head}
-            face={face}
-            accessory={accessory}
-            skinColor={skinColor}
-            clothesColor={clothesColor}
-          />
-        </div>
       </MaxWidthWrapper>
+
+      <div className={styles.characterWrapper}>
+        <Character
+          body={body}
+          head={head}
+          face={face}
+          accessory={accessory}
+          skinColor={skinColor}
+          clothesColor={clothesColor}
+        />
+      </div>
     </main>
   );
 }
